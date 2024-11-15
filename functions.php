910acd29b80dc035194de21b769d15b97fd9ea4d
Petya Naydenova <petyanayde@gmail.com>
2024-11-15 09:14:49 +0200

    adding additional files

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 function storefront_child_theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child_style', get_theme_file_uri('assets/dist/main.css'));
 }
 add_action('wp_enqueue_scripts', 'storefront_child_theme_enqueue_styles');
